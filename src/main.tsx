d72462b4fa55262170fd00e34496bbe4e0fd1837
cristian.mendez <cristian.mendez@globant.com>
2024-07-03 12:44:08 +0000

    -initial setting + router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { LayoutMain } from "./components/layouts/LayoutMain";
 import "./index.css";
-import { App } from './App';
+import { Cart } from "./pages/Cart/Cart";
+import { Home } from "./pages/Home/Home";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <LayoutMain />,
+		children:[
+			{
+				index: true,
+				element: <Home />
+			},
+			{
+				path: "/cart",
+				element: <Cart />
+			}
+		]
+	},
+])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		{/* <App /> */}
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
